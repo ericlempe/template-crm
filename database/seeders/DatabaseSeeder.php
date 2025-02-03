e1be99d40c9a859e3624d28c83f10469bcc0c08f
ericlempe <dev.ericlp@gmail.com>
2025-02-03 07:21:12 -0300

    add opportunities factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Opportunity;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             UserSeeder::class,
             CustomerSeeder::class,
+            Opportunity::class,
         ]);
 
     }

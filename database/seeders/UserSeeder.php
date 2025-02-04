6a406b85064d892da97c8619d810095a055ff2d2
ericlempe <dev.ericlp@gmail.com>
2025-02-03 22:21:40 -0300

    optimize user factory and seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Can;
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -12,14 +14,45 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()
-            ->admin()
+        $admin = User::factory()
+            ->withPermission(Can::BE_AN_ADMIN)
             ->create([
                 'name'  => 'Admin',
                 'email' => 'admin@email.com',
             ]);
 
-        User::factory(20)->create();
-        User::factory(5)->deleted()->create();
+        $this->normalUsers();
+        $this->deletedUsers($admin);
+    }
+
+    private function defaultDefinition(): array
+    {
+        return array_merge((new UserFactory())->definition(), ['password' => '$2y$10$Ybe7g6ojOtQLVDmX914YUeCqdpfKmuOkPlA9n0zvH.3HLO0u0PA56']);
+    }
+
+    private function normalUsers(): void
+    {
+        User::query()->insert(
+            array_map(
+                fn () => $this->defaultDefinition(),
+                range(1, 50)
+            )
+        );
+    }
+
+    private function deletedUsers(User $admin): void
+    {
+        User::query()->insert(
+            array_map(
+                fn () => array_merge(
+                    $this->defaultDefinition(),
+                    [
+                        'deleted_at' => now(),
+                        'deleted_by' => $admin->id,
+                    ]
+                ),
+                range(1, 50)
+            )
+        );
     }
 }

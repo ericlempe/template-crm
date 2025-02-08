16e9a3869c2188e37f2f5e78c1b81c7192918f3d
ericlempe <dev.ericlp@gmail.com>
2025-02-08 10:14:22 -0300

    it should be able to update status and order of each opportunity

--- resources/views/components/board/group.blade.php
+++ resources/views/components/board/group.blade.php
@@ -7,12 +7,12 @@
     <x-header :title="ucfirst($status)" :subtitle="'Total ' . $items->count()  . ' opportunities'" size="text-xl"
               separator progress-indicator/>
     <div class="space-y-2 p-2" wire:sortable-group.item-group="{{ $status }}">
-        @empty($items)
+        @if($items->isEmpty())
             <div wire:key="opportunity-null"
                  class="h-10 border-dashed border-gray-400 border-2 shadow rounded w-full flex items-center justify-center opacity-40">
                 Empty List
             </div>
-        @endempty
+        @endif
         @foreach($items as $item)
             <x-card
                 class="hover:opacity-60 cursor-grab"
